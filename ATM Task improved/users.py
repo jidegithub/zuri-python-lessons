b648f1a8f5688ac2e9aa76e67be5fee5bdad6052
jidegithub <olutolajide@gmail.com>
2021-04-12 21:23:11 +0100

    update: updated register user module

--- ATM Task improved/users.py
+++ ATM Task improved/users.py
@@ -1,38 +1,11 @@
 import json
 
-users = '''
-{
-  "users":[
-  {
-    "id": "6935684884",
-    "email": "m.lawal@gmail.com",
-    "first_name": "morire",
-    "last_name": "lawal",
-    "password": "mlawal",
-    "account_number": 1326008519
-  },
-  {
-    "id": "8725028055",
-    "email": "p.ikechukwu@gmail.com",
-    "first_name": "permanent",
-    "last_name": "ikechuwku",
-    "password": "pikechukwu",
-    "account_number": 1998001066
-  },
-  {
-    "id": "8076679877",
-    "email": "h.alaga@gmail.com",
-    "first_name": "hafusat",
-    "last_name": "alaga",
-    "password": "halaga",
-    "account_number": 2238238543
-  }
-  ]
-}
-'''
+with open('./users.json') as user_json_file:
+  datalist = json.load(user_json_file)
+  # datalist = (data['users'])
 
-data = json.loads(users)
-datalist = (data['users'])
+# print(datalist)
+
 current_user = ""
 
 def does_account_number_exist(account_number):
@@ -53,17 +26,22 @@ def authenticated_user(account_number, password):
       print('password incorrect')
   else:
     return False
+    
+def append_list(data):
+  datalist.append(data)
+  with open('users.json', 'w') as outfile:
+    json.dump(datalist, outfile)
 
 def create(account_number, first_name, last_name, email, password, iden):
-  data = {
+  new_data = {
     "id": iden,
     "email": email,
     "first_name": first_name,
     "last_name": last_name,
     "password": password,
     "account_number": account_number
-  },
+  }
+  append_list(new_data)
+  return True
 
-  with open('users.json', 'w') as outfile:
-    json.dump(data, outfile)
-    return True
+  
